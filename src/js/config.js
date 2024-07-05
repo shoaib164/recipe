5919f5e90ff9e85e0c0f70de06be10e50dc2a79e
kuldeep <kuldeepgautam06082001@gmail.com>
2024-07-05 19:59:45 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 13;
+export const KEY = '116e6dfe-1c17-4c0e-8259-18b2222efcb7';
+export const MODAL_CLOSE_SEC = 2.5;
